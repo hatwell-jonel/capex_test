37dde5c0d0f4c27bd9e03a376d1ea019bc08a60c
Jonel Hatwell <jonel.hatwell@gmail.com>
2024-06-27 09:10:32 +0800

    commmmit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
-     <h1 className="text-2xl">try to deploy staging</h1>
+     <h1 className="text-2xl">try to deploy staging branch</h1>
     </div>
   );
 }
